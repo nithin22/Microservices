e35f485992dd0a9603b690d542831418b21a0911
Venkat Nithin Atturu <atturunithin@gmail.com>
2023-03-09 18:10:15 -0600

    first

---
+++ src/main/java/com/nithin/microsrvices/currencyexchanageservice/CurrencyExchanageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nithin.microsrvices.currencyexchanageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchanageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchanageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nithin/microsrvices/currencyexchanageservice/CurrencyExchanageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nithin.microsrvices.currencyexchanageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyExchanageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
